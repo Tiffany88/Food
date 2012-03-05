26769a47abd29ff38e6ac9c2dbeafafa5c2c8a30
Tiffany Eddleman <tiffany.eddleman@MAED-012.local>
2012-03-05 08:25:17 -0700

    Initial Commit

---
+++ TrailStuff/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TrailStuff
+//
+//  Created by Tiffany Eddleman on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TrailStuff/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  TrailStuff
+//
+//  Created by Tiffany Eddleman on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
